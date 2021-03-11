97bb02d3f430d6ac07e660766300ab222d5ed809
Adrien Lapasset <aflapasset@gmail.com>
2021-03-11 17:31:29 +0100

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import breakpoint from 'styled-components-breakpoint';
 
 const Footer = () => {
 
@@ -7,7 +8,7 @@ const Footer = () => {
 
   return (
     <StyleContainer>
-      © {currentYear} Adrien Lapasset
+      Made with <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a> and <span>❤️</span> <StyledBr />© {currentYear} Adrien Lapasset
     </StyleContainer>
   );
 }
@@ -16,6 +17,18 @@ const StyleContainer = styled.section`
   color: ${props => props.theme.colors.grey};
   text-align: center;
   margin-bottom: 15px;
+  a {
+    color: ${props => props.theme.colors.grey};
+    font-size: 16px;
+  }
+  span {
+    font-size: 12px;
+  }
+`
+const StyledBr = styled.br`
+  ${breakpoint('sm')`
+    display: none;
+  `}
 `
 
 export default Footer;
