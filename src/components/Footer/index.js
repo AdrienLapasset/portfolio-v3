9083eb512d1cee0915d2891ea9702db3a1cfd4a0
Adrien Lapasset <aflapasset@gmail.com>
2021-03-11 17:41:52 +0100

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ const Footer = () => {
 
   return (
     <StyleContainer>
-      Made with <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a> and <span>❤️</span> <StyledBr />© {currentYear} Adrien Lapasset
+      Made with <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a> and <StyledHeart>❤️</StyledHeart><StyledBr /><StyledAuthor>© {currentYear} Adrien Lapasset</StyledAuthor>
     </StyleContainer>
   );
 }
@@ -17,17 +17,25 @@ const StyleContainer = styled.section`
   color: ${props => props.theme.colors.grey};
   text-align: center;
   margin-bottom: 15px;
+  font-size: 14px;
   a {
     color: ${props => props.theme.colors.grey};
-    font-size: 16px;
+    font-size: 14px;
   }
-  span {
-    font-size: 12px;
-  }
+`
+const StyledHeart = styled.span`
+  font-size: 10px;
 `
 const StyledBr = styled.br`
   ${breakpoint('sm')`
     display: none;
+  `}
+`
+const StyledAuthor = styled.span`
+  display: inline-block;
+  margin-top: 3px;
+  ${breakpoint('sm')`
+    margin-left: 10px;
   `}
 `
 
